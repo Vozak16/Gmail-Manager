d9f7ee32b55e719fd685d4281ab16fb1c0178465
yar.mor <ta.tak.hopm@gmail.com>
2020-05-19 19:26:10 +0300

    some changes

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/yar.mor/Documents/JetBrains/Python/[Course №1]/[Semester 2]/[Coursework]Gmail Manager/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
